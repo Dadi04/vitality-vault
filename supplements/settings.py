2359eddb711b36066912d7676268129aa3b6d032
David Petkovic <dadica.petkovic@gmail.com>
2025-03-13 13:27:15 +0100

    migration

--- supplements/settings.py
+++ supplements/settings.py
@@ -126,15 +126,15 @@ WSGI_APPLICATION = 'supplements.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.postgresql',
-    #     'NAME': env('DB_NAME'),
-    #     'USER': env('DB_USER'),
-    #     'PASSWORD': env('DB_PASSWORD'),
-    #     'HOST': env('DB_HOST'),
-    #     'PORT': env('DB_PORT'),
-    # },
-    'default': dj_database_url.config(default=env('DATABASE_URL'))
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': env('DB_NAME'),
+        'USER': env('DB_USER'),
+        'PASSWORD': env('DB_PASSWORD'),
+        'HOST': env('DB_HOST'),
+        'PORT': env('DB_PORT'),
+    },
+    # 'default': dj_database_url.config(default=env('DATABASE_URL'))
 }
 
 # Email configuration 
