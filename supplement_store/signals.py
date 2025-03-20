73a4b248ff2bc713c32c19dcc12065a3da54a888
David Petkovic <dadica.petkovic@gmail.com>
2025-03-20 10:38:58 +0100

    added so that after logging in with google/facebook, the cart/wishlist stay the same, updated README.md

--- supplement_store/signals.py
+++ supplement_store/signals.py
@@ -2,8 +2,10 @@ from django.dispatch import receiver
 from django.shortcuts import get_object_or_404
 
 from paypal.standard.ipn.signals import valid_ipn_received
+from allauth.account.signals import user_logged_in
 
 from .models import Transaction
+from .shop_utils import merge_carts, merge_wishlists
 
 @receiver(valid_ipn_received)
 def paypal_ipn_handler(sender, **kwargs):
@@ -13,3 +15,8 @@ def paypal_ipn_handler(sender, **kwargs):
         if transaction.total_price == ipn.mc_gross:
             transaction.status = 'paid'
             transaction.save()
+
+@receiver(user_logged_in)
+def merge_user_data_on_login(request, user, **kwargs):
+    merge_carts(request, user)
+    merge_wishlists(request, user)
